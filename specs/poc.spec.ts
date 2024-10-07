73f61aa1a6000d12df77a10f2e971810a18ae16c
pabs28 <pablosapitan28@gmail.com>
2024-10-08 00:01:51 +0800

    Initial commits and add test

---
+++ specs/poc.spec.ts
@@ -0,0 +1,11 @@
+import * as supertest from "supertest";
+const request = supertest('https://jsonplaceholder.typicode.com/');
+
+describe('POC Tests', () => {
+    it('GET /posts', async () => {
+        const res = await request.get('/posts');
+        console.log(res);
+        expect(res.statusCode).toBe(200);
+        expect(res.body[0].id).toBe(1)
+    })
+})
